623a4ec6f75ef21e8b6cc0821623d91d6ca47832
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2019-01-22 19:12:23 +0600

    settings design added

--- popup.js
+++ popup.js
@@ -15,6 +15,55 @@ if(topBtnSel) {
         var element = document.getElementById("topSites");
         element.classList.toggle("showHide");
     })
+    topBtnSel.addEventListener('click', function(){
+        var element = document.getElementById("topSites");
+        element.classList.toggle("showHide");
+    })
 }
+var set = document.getElementById('plus')
+if(set) {
+    set.addEventListener('click', function(){
+
+        var element1 = document.getElementById("main-body");
+        var element2 = document.getElementById("settings");
+        var element = document.getElementById("topSites");
+        element.classList.add("showHide");
+        element1.classList.toggle("showHide");
+        element2.classList.toggle("showHide");
+
+
+    })
+}
+var sel = document.getElementById("setLogo")
+if(sel){
+    sel.addEventListener('change',srcIcon)
+}
+
+function srcIcon(){
+    var opt = sel.options[sel.selectedIndex].value;
+    if(opt == "link1"){
+        document.getElementById("customLogo").src ="images/img_1.png"
+    }
+    if (opt == "link2"){
+        document.getElementById("customLogo").src ="images/img_2.png"
+    }
+    if (opt == "link3"){
+        document.getElementById("customLogo").src ="images/img_3.png"
+    }
+    if (opt == "link4"){
+        document.getElementById("customLogo").src ="images/img_4.png"
+    }
+    if (opt == "link5"){
+        document.getElementById("customLogo").src ="images/img_5.png"
+    }
+}
+var newLink =document.getElementById("main-body-bottom")
+    if(newLink){
+        newLink.innerHTML =`  <a title="add new" href="" target="_blank"><img src="images/img_1.png" class="icon-logo"></a>
+    <a title="add new" href="" target="_blank"><img src="images/img_2.png" class="icon-logo"></a>
+    <a title="add new" href="" target="_blank"><img src="images/img_3.png" class="icon-logo"></a>
+    <a title="add new" href="" target="_blank"><img src="images/img_4.png" class="icon-logo"></a>
+    <a title="add new" href="" target="_blank"><img src="images/img_5.png" class="icon-logo"></a>`
+    }
 
 
