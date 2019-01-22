38346b3732409a49a59924c26e6b3eb35f29e4ca
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2019-01-22 12:09:20 +0600

    version 1.0.5

---
+++ popup.js
@@ -0,0 +1,17 @@
+chrome.topSites.get(function (top){
+    if(top){
+        var str = '<ul>'
+        top.forEach(data=>{
+            var name = data.title;
+            var link = data.url;
+            str += `<li><a target="_blank" href="${link}">${name}</a></li>`
+        })
+        document.getElementById('topSites').innerHTML = str;
+    }
+})
+var topBtn = document.getElementById('topBtn')
+topBtn.addEventListener('click', function(){
+    var element = document.getElementById("topSites");
+    element.classList.toggle("showHide");
+})
+
